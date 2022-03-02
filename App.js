455ff455fff95177d5912239c631bcb1c92850a6
miru-porto <portocarreromiranda@gmail.com>
2022-03-02 13:29:11 -0300

    agregando texto

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
-import {View, Button, TextInput} from 'react-native';
+import {Text, View, Button, TextInput} from 'react-native';
 
 import {styles} from './styles';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder="Type here"
-        placeholderTextColor={'#841584'}
-        autoCorrect={false}
-        autoFocus={true}
-        style={styles.textInput}
-      />
+    <>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type here"
+          placeholderTextColor={'#841584'}
+          autoCorrect={false}
+          autoFocus={true}
+          style={styles.textInput}
+        />
 
-      <Button title="Add" color="#841584" onPress={() => null} />
-    </View>
+        <Button title="Add" color="#841584" onPress={() => null} />
+      </View>
+
+      <View>
+        <Text style={styles.textH1}> Más populares</Text>
+      </View>
+    </>
   );
 };
 
--- styles.js
+++ styles.js
@@ -1,20 +1,25 @@
-import { StyleSheet } from 'react-native'
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-      padding: 25,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    textInput: {
-      borderColor: 'black',
-      borderBottomWidth: 1,
-      marginBottom: 10,
-      width: '70%',
-      height: 40,
-      fontSize: 14,
-      color: '#212121',
-    },
-  });
-  
+  container: {
+    padding: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderColor: 'black',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    width: '70%',
+    height: 40,
+    fontSize: 14,
+    color: '#212121',
+  },
+  textH1: {
+    width: '100%',
+    color: '#000000',
+    //borderWidth: 1,
+    textAlign: "center",
+  },
+});
