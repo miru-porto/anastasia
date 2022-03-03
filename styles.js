0a55eac578a77a3a5fbf5860b173ff64afbda29b
miru-porto <portocarreromiranda@gmail.com>
2022-03-02 21:53:25 -0300

    agregando modal

--- styles.js
+++ styles.js
@@ -2,11 +2,15 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  containerInput: {
     padding: 25,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  containerList: {paddingHorizontal: 25},
   textInput: {
     borderColor: 'black',
     borderBottomWidth: 1,
@@ -20,6 +24,39 @@ export const styles = StyleSheet.create({
     width: '100%',
     color: '#000000',
     //borderWidth: 1,
-    textAlign: "center",
+    textAlign: 'center',
+  },
+
+  textList: {fontSize: 14, color: '#212121', marginBottom: 10},
+
+  modalContainerContent: {
+    width: '80%',
+    height: '30%',
+    elevation: 4,
+    justifyContent: 'center',
+    borderRadius: 4,
+    marginLeft: 40,
+    marginTop: 200,
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+  },
+  modaTitle: {
+    fontSize: 14,
+    color: '#212121',
+    padding: 25,
+  },
+  modalContent: {
+    paddingHorizontal: 25,
+  },
+  modalContentText: {
+    fontSize: 14,
+    color: '#212121',
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  modalButton: {
+    marginVertical: 10,
+    paddingHorizontal: 25,
   },
 });
